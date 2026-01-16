102a3209a0df2aff9c99809af21e7081ccf265b3
shubhambhattacharya-dev <get.input.2025@gmail.com>
2026-01-16 18:54:34 +0530

    navbar completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # HealthCare
+
+today 15 jan :-
+
+today i setUp my frontend vite and tailwindcss  project successfully .
+
+dependecies used :- In frontend
+1. vite
+2. react
+3. tailwindcss
+4. axios
+5. react-router-dom
+6. react-icons
+7. react-hot-toast
+8. zustand
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import DoctorPage from './pages/DoctorPage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import ContactPage from './pages/contactPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import MyAppointmentPage from './pages/MyAppooitmentPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import Navbar from './components/Navbar.jsx'
+import Appointment from './pages/Appointment.jsx'
 
 const App = () => {
   return (
-    <div className='bg-red-700 text-3xl font-bold underline'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/doctors" element={<DoctorPage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/contact" element={<ContactPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/my-appointments" element={<MyAppointmentPage/>}/>
+      <Route path="appointment/:docId" element={<Appointment/>}/>
+      <Route path="/Profile" element={<ProfilePage/>}/>
+     
+      </Routes>
+      </div>
   )
-}
+  }
+
+
+
 
 export default App
