fa05424e0bff8e258639e5eff944f90e3331f5f7
shubhambhattacharya-dev <get.input.2025@gmail.com>
2026-01-23 11:08:14 +0530

    completed ui almost

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,101 @@
 import React from 'react'
+import { useState } from 'react'
 
 const LoginPage = () => {
+
+  const [state, setState] = useState('Sign Up')
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>LoginPage</div>
+    <form
+      className='min-h-[80vh] flex justify-center'
+      onSubmit={onSubmitHandler}
+    >
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+
+        <p className='text-2xl font-semibold'>
+          {state === 'Sign Up' ? 'Create an Account' : 'Login'}
+        </p>
+
+        <p>
+          Please {state === 'Sign Up' ? 'Create an Account' : 'Login'} to book appointment
+        </p>
+
+        {state === 'Sign Up' && (
+          <div className='w-full'>
+            <p className='font-semibold'>Full Name</p>
+            <input
+              className='border border-zinc-300 rounded w-full p-2 mt-1'
+              type='text'
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
+          </div>
+        )}
+
+        <div className='w-full'>
+          <p className='font-semibold'>Email</p>
+          <input
+            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+
+        <div className='w-full'>
+          <p className='font-semibold'>Password</p>
+          <input
+            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button
+          className='bg-primary text-white p-2 w-full py-2 rounded-md text-base'
+          type='submit'
+        >
+          {state === 'Sign Up'
+            ? 'Create Account'
+            : 'Login'}
+        </button>
+
+        {state === 'Sign Up' ? (
+          <p>
+            Already have an account?{' '}
+            <span
+              onClick={() => setState('Login')}
+              className='text-primary underline cursor-pointer'
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create a new account?{' '}
+            <span
+              onClick={() => setState('Sign Up')}
+              className='text-primary underline cursor-pointer'
+            >
+              Click here
+            </span>
+          </p>
+        )}
+
+      </div>
+    </form>
   )
 }
 
--- frontend/src/pages/MyAppointmentPage.jsx
+++ frontend/src/pages/MyAppointmentPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {AppContext} from '../context/AppContext'
 
 const MyAppointmentPage = () => {
+  const {doctors}=useContext(AppContext)
   return (
     <div>MyAppointmentPage</div>
   )
