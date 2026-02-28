591671efd13391dd8e57775802890c8cfdb706f2
shubhambhattacharya-dev <get.input.2025@gmail.com>
2026-02-28 22:21:29 +0530

    connect with postgress and role based entry setups

--- components/header.jsx
+++ components/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {
+  SignIn,
   SignInButton,
   SignUpButton,
   SignedIn,
@@ -9,8 +10,15 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import { Button } from './ui/button'
+import { checkUser } from '@/lib/checkUser'
+import { Calendar, User } from 'lucide-react'
 
-const Header = () => {
+
+const Header = async() => {
+
+const user =await checkUser();
+
+
   return (
     <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-sm z-10 supports-[backdrop-filter]:bg-background/80'>
       <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
@@ -25,6 +33,61 @@ const Header = () => {
         </Link>
 
         <div className='flex items-center space-x-2'>
+
+         <SignedIn>
+
+          
+
+         {/* patient */}
+{user?.role === "PATIENT" && (
+  <Link href="/appointment">
+    <Button
+      variant="outline"
+      className="hidden md:inline-flex items-center gap-2"
+    >
+      <Calendar className="h-4 w-4" />
+      My Appointments
+    </Button>
+
+    <Button
+      variant="ghost"
+      className="md:hidden w-10 h-10 p-0"
+    >
+      <Calendar className="h-4 w-4" />
+    </Button>
+  </Link>
+)}
+
+          
+
+
+
+
+
+
+          {/* unassigned */}
+  {user?.role === "UNASSIGNED" && (
+    <Link href="/onboarding">
+      
+      <Button
+        variant="outline"
+        className="hidden md:inline-flex items-center gap-2"
+      >
+        <User className="h-4 w-4" />
+        Complete Profile
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="md:hidden w-10 h-10 p-0"
+      >
+        <User className="h-4 w-4" />
+      </Button>
+
+    </Link>
+  )}
+</SignedIn>
+
           <SignedOut>
             <SignInButton >
            
