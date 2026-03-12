0ec23bfdcf8d9cfaa057f75149f6198d303f3419
shubhambhattacharya-dev <get.input.2025@gmail.com>
2026-03-12 23:19:01 +0530

    avalability

---
+++ app/(main)/doctor/_components/doctorDashboard.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import AvailableSlots from "./availableSlots";
+import { Calendar, Clock, DollarSign } from "lucide-react";
+import Appointments from "./appointments";
+import Payouts from "./payouts";
+
+export default function DoctorDashboard({
+  appointmentsData,
+  availabilityData,
+  earningsData,
+  payoutsData,
+}) {
+  return (
+    <Tabs
+      defaultValue="earnings"
+      className="grid grid-cols-1 md:grid-cols-4 gap-6"
+    >
+      <TabsList className="md:col-span-1 bg-muted/30 border h-14 md:h-40 flex sm:flex-row md:flex-col w-full p-2 md:p-1 rounded-md md:space-y-2 sm:space-x-2 md:space-x-0">
+        <TabsTrigger
+          value="earnings"
+          className="flex-1 md:flex md:items-center md:justify-start md:px-4 md:py-3 w-full"
+        >
+          <DollarSign className="h-4 w-4 mr-2 hidden md:inline" />
+          <span>Earnings</span>
+        </TabsTrigger>
+        <TabsTrigger
+          value="appointments"
+          className="flex-1 md:flex md:items-center md:justify-start md:px-4 md:py-3 w-full"
+        >
+          <Calendar className="h-4 w-4 mr-2 hidden md:inline" />
+          <span>Appointments</span>
+        </TabsTrigger>
+        <TabsTrigger
+          value="availability"
+          className="flex-1 md:flex md:items-center md:justify-start md:px-4 md:py-3 w-full"
+        >
+          <Clock className="h-4 w-4 mr-2 hidden md:inline" />
+          <span>Availability</span>
+        </TabsTrigger>
+      </TabsList>
+      <div className="md:col-span-3">
+        <TabsContent value="appointments" className="border-none p-0">
+          <Appointments
+            appointments={appointmentsData.appointments || []}
+          />
+        </TabsContent>
+        <TabsContent value="availability" className="border-none p-0">
+          <AvailableSlots slots={availabilityData.slots || []} />
+        </TabsContent>
+        <TabsContent value="earnings" className="border-none p-0">
+          <Payouts
+            payouts={payoutsData.payouts || []}
+          />
+        </TabsContent>
+      </div>
+    </Tabs>
+  );
+}
